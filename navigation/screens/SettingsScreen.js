b5a6e030948772d69ee3f1d9af1300f86ab7f9e2
Tessa <tess.starkweather@gmail.com>
2023-05-03 14:50:15 -0400

    add basic home screen layout

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -3,6 +3,14 @@ import { View, Text } from 'react-native';
 
 export default function SettingsScreen({ navigation }) {
     return (
+// top 1/2 of page = box with background, user icon, and user name
+// bottom 1/2 = clickable ListItems
+        // username settings
+        // phone number settings
+        // notification settings
+        // privacy settings
+
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text
                 onPress={() => alert('This is the "Home" screen.')}
